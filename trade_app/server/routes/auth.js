53e5d2f6d402b63b1a853267b06aa6f4aef6a280
doriaaaa_ <doria.tang333@gmail.com>
2022-11-21 01:53:03 +0800

    added book search function

--- trade_app/server/routes/auth.js
+++ trade_app/server/routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const User = require("../models/user");
+const isbn = require('node-isbn');
+// const Book = require("../models/book_info");
 
 const authRouter = express.Router();
 
@@ -7,7 +9,17 @@ authRouter.get("/hello-world", (req, res) => {
     res.json({
         hi: "hello world"
     });
-})
+});
+
+authRouter.post("/api/bookinfo", async (req, res) => {
+    const { book_isbn } = req.body;
+
+    isbn.resolve(book_isbn).then(function (book) {
+        return res.json(book);
+    }).catch(function (err) {
+        res.status(500).json({ error: err });
+    });
+});
 
 authRouter.post("/api/signup", async (req, res) => {
     try {
