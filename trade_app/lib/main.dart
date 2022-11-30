bd7f3ab6cf555df36e6abf8407be94a3c8391ccf
LabRat404 <44639057+LabRat404@users.noreply.github.com>
2022-11-30 18:24:38 +0800

    Added settings page
    User can logout and check for "about us" in settings, also upgraded some packages

--- trade_app/lib/main.dart
+++ trade_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:trade_app/screens/home_page.dart';
 import '../screens/login_page.dart';
+import '../screens/settings_page.dart';
 import 'package:trade_app/routes/router.dart';
 // import '../widgets/nav_bar.dart';
 
@@ -15,25 +16,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Trade App',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-        ),
-        // home: const NavBar(),
-        // home: LoginPage(),
-        onGenerateRoute: (settings) => generateRoute(settings),
-        home: LoginPage()
-        //home: InfoPage(),
-        //home: HomePage(),
-        );
+      title: 'Trade App',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      // home: const NavBar(),
+      home: LoginPage(),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      //home: InfoPage(),
+      //home: HomePage(),
+      //home: SettingsPage(),
+    );
   }
 }
--- trade_app/lib/screens/settings_page.dart
+++ trade_app/lib/screens/settings_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:babstrap_settings_screen/babstrap_settings_screen.dart';
+import 'package:trade_app/screens/information_page.dart';
+import 'package:trade_app/screens/login_page.dart';
+import 'package:trade_app/widgets/app_title_homepage.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -8,17 +14,110 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: const Text('this is settings page'),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white.withOpacity(.94),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: [
+              // user card
+              SimpleUserCard(
+                userName: "Admin124",
+                userProfilePic: AssetImage("assets/avator.jpg"),
+              ),
+              SettingsGroup(
+                items: [
+                  SettingsItem(
+                    onTap: () {},
+                    icons: CupertinoIcons.pencil_outline,
+                    iconStyle: IconStyle(),
+                    title: 'Appearance',
+                    subtitle: "Change your avator!",
+                  ),
+                  SettingsItem(
+                    onTap: () {},
+                    icons: Icons.fingerprint,
+                    iconStyle: IconStyle(
+                      iconsColor: Colors.white,
+                      withBackground: true,
+                      backgroundColor: Colors.red,
+                    ),
+                    title: 'Privacy',
+                    subtitle: "Choose what to share!",
+                  ),
+                  SettingsItem(
+                    onTap: () {},
+                    icons: Icons.dark_mode_rounded,
+                    iconStyle: IconStyle(
+                      iconsColor: Colors.white,
+                      withBackground: true,
+                      backgroundColor: Colors.red,
+                    ),
+                    title: 'Dark mode',
+                    subtitle: "Automatic",
+                    trailing: Switch.adaptive(
+                      value: false,
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+              SettingsGroup(
+                items: [
+                  SettingsItem(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const InformationPage()),
+                      );
+                    },
+                    icons: Icons.info_rounded,
+                    iconStyle: IconStyle(
+                      backgroundColor: Colors.purple,
+                    ),
+                    title: 'About',
+                    subtitle: "Learn more Trade Books",
+                  ),
+                ],
+              ),
+              // You can add a settings title
+              SettingsGroup(
+                settingsGroupTitle: "Account",
+                items: [
+                  SettingsItem(
+                    onTap: () {
+                      Navigator.of(context).pushAndRemoveUntil(
+                          new MaterialPageRoute(
+                              builder: (context) => new LoginPage()),
+                          (route) => false);
+                    },
+                    icons: Icons.exit_to_app_rounded,
+                    title: "Sign Out",
+                  ),
+                  SettingsItem(
+                    onTap: () {},
+                    icons: CupertinoIcons.repeat,
+                    title: "Change email",
+                  ),
+                  SettingsItem(
+                    onTap: () {},
+                    icons: CupertinoIcons.delete_solid,
+                    title: "Delete account",
+                    titleStyle: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      body:Container(
-        child: Text('this is settings page'),
-      )
     );
   }
 }
