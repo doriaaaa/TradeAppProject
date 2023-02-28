6fef27e01341245ed46218cf9f7ab375a6392b68
doriaaaa_ <doria.tang333@gmail.com>
2023-02-28 15:53:00 +0800

    remove double awaits

--- trade_app/server/routes/upload.js
+++ trade_app/server/routes/upload.js
@@ -8,25 +8,20 @@ const {parse, stringify, toJSON, fromJSON} = require('flatted');
 uploadRouter.post("/api/user/upload", auth, async (req, res) => {
     // const { title, author, publisher, pics_url, description } = req.body;
     const bookToAdd = req.body;
-    let user = await User.findById(req.user);
-    // console.log(stringify(user));
+    // let user = await User.findById(req.user);
+    console.log(bookToAdd);
+    console.log(req.user);
     // console.log(user.name);
     try {
-        if (!user) {
-            return res.status(400).json({
-                msg: "Error in finding user"
-            });
-        } else {
-            user = await User.findOneAndUpdate(user, { $push: {uploadedBookList: bookToAdd} }, function(err, res) {
-                if(err) {
-                    console.log(err);
-                    console.log(res);
-                    console.log("failed to save the book");
-                } else {
-                    console.log("book is uploaded successfully");
-                }
-            });
-        }
+        const user = await User.findOneAndUpdate({_id: req.user}, { $push: {uploadedBookList: bookToAdd} }, function(err, res) {
+            if(err) {
+                console.log(err);
+                console.log(res);
+                console.log("failed to save the book");
+            } else {
+                console.log("book is uploaded successfully");
+            }
+        });
     } catch (e) {
         return res.status(500).json({ error: e.message });
     }
